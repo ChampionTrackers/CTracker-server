7c4156163c7e48f4100844d32d9b50c4be2f3995
Erick Sousa <16109127+ericksantos12@users.noreply.github.com>
2024-04-22 21:32:10 -0300

    ✨ add get profile route

---
+++ src/routes/users/get-profile.ts
@@ -0,0 +1,60 @@
+import { prisma } from '@/lib/prisma'
+import { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import z from 'zod'
+import { NotFoundError } from '../_errors/NotFound'
+
+export async function getProfile(app: FastifyInstance) {
+    app.withTypeProvider<ZodTypeProvider>().get(
+        '/profile',
+        {
+            preHandler: [app.authenticate],
+            schema: {
+                summary: 'Get your profile',
+                tags: ['User'],
+                response: {
+                    200: z.object({
+                        user: z.object({
+                            id: z.number().int(),
+                            email: z.string().email(),
+                            name: z.string(),
+                            nickname: z.string(),
+                            picture: z.string().url().nullable(),
+                            score: z.number().int(),
+                        })
+                    }),
+                },
+            },
+        },
+        async (request, reply) => {
+            const { id } = request.user
+
+            const user = await prisma.user.findUnique({
+                select: {
+                    id: true,
+                    email: true,
+                    name: true,
+                    nickname: true,
+                    picture: true,
+                    score: true,
+                },
+                where: {
+                    id,
+                }
+            })
+
+            if (user === null) {
+                throw new NotFoundError("User not found")
+            }
+
+            return reply.send({ user: {
+                id: user.id,
+                email: user.email,
+                name: user.name,
+                nickname: user.nickname,
+                picture: user.picture,
+                score: user.score
+            } })
+        }
+    )
+}
