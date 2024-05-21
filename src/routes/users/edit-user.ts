ea0839c783ac6ddcfd31c00433f3732a9c4a79e9
Erick Sousa <16109127+ericksantos12@users.noreply.github.com>
2024-05-21 19:30:13 -0300

    ♻️ add password requirement and validations

--- src/routes/users/edit-user.ts
+++ src/routes/users/edit-user.ts
@@ -1,46 +1,117 @@
-import { prisma } from "@/lib/prisma";
-import { verifyJwt } from "@/middlewares/verifyJWT";
-import { generateHash } from "@/utils/hash";
-import { FastifyInstance } from "fastify";
-import { ZodTypeProvider } from "fastify-type-provider-zod";
-import z from "zod";
+import { prisma } from '@/lib/prisma';
+import { verifyJwt } from '@/middlewares/verifyJWT';
+import { compareHash, generateHash } from '@/utils/hash';
+import { FastifyInstance } from 'fastify';
+import { ZodTypeProvider } from 'fastify-type-provider-zod';
+import z from 'zod';
+import { NotFoundError } from '../_errors/NotFound';
+import { UnauthorizedError } from '../_errors/Unauthorized';
+import { ConflictError } from '../_errors/Conflict';
 
-export async function editUser(app: FastifyInstance){
-  app.withTypeProvider<ZodTypeProvider>().patch("/users", {
-    onRequest: (request, reply) => verifyJwt(request, reply),
-    schema: {
-      summary: "Update your profile",
-      tags: ["User"],
-      security: [{ JWT: [] }],
-      body: z.object({
-        email: z.string().email().optional(),
-        name: z.string().min(3).max(50).optional(),
-        nickname: z.string().min(3).max(20).optional(),
-        password: z.string().min(8).optional(),
-        picture: z.string().url().optional(),
-      }),
-      response: {
-        200: z.null()
-      }
-    }
-  }, async (request, reply) => {
-    const userId = request.user.id;
-    const user = request.body;
-    
-    if(user.password){
-      user.password = await generateHash(user.password);
-    }
-    
-    const createUser = await prisma.user.update({
-      where: {
-        id: userId
+export async function editUser(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().patch(
+    '/users',
+    {
+      onRequest: (request, reply) => verifyJwt(request, reply),
+      schema: {
+        summary: 'Update your profile',
+        tags: ['Auth'],
+        security: [{ JWT: [] }],
+        body: z.object({
+          data: z.object({
+            email: z.string().email().optional(),
+            name: z.string().min(3).max(50).optional(),
+            nickname: z
+              .string()
+              .min(3)
+              .max(20)
+              .refine(
+                (string) => !string.includes(' '),
+                'Nickname should not have spaces'
+              )
+              .optional(),
+            picture: z.string().url().optional(),
+          }),
+          password: z.string().min(8),
+        }),
+        response: {
+          200: z.object({
+            token: z.string(),
+          }),
+        },
       },
-      data: {
-        ...user
-      }
-    })
+    },
+    async (request, reply) => {
+      const userId = request.user.id;
+      const data = request.body.data;
+      const password = request.body.password;
 
-    return reply.status(204).send();
-    
-  })
+      const userPassword = await prisma.user.findUnique({
+        where: {
+          id: userId,
+        },
+        select: {
+          password: true,
+        },
+      });
+
+      if (userPassword === null) {
+        throw new NotFoundError('User not found');
+      }
+
+      const passwordCompare = await compareHash(
+        password,
+        userPassword.password
+      );
+
+      if (!passwordCompare) {
+        throw new UnauthorizedError('Wrong Password');
+      }
+
+      if (data.nickname) {
+        const findNickname = await prisma.user.findUnique({
+          where: {
+            nickname: data.nickname,
+          },
+        });
+
+        if (findNickname) {
+          throw new ConflictError('Nickname already exists');
+        }
+      }
+
+      if (data.email) {
+        const findEmail = await prisma.user.findUnique({
+          where: {
+            email: data.email,
+          },
+        });
+
+        if (findEmail) {
+          throw new ConflictError('Email already exists');
+        }
+      }
+
+      await prisma.user.update({
+        where: {
+          id: userId,
+        },
+        data,
+      });
+
+      const newUser = await prisma.user.findUnique({
+        where: {
+          id: userId,
+        },
+      });
+
+      const token = await reply.jwtSign({
+        id: newUser!.id,
+        email: newUser!.email,
+        nickname: newUser!.nickname,
+      });
+
+      return reply.status(200).send({ token });
+    }
+  );
 }
