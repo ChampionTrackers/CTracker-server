7acae64126212c1454d3e28316912156e5fb915a
Erick Sousa <16109127+ericksantos12@users.noreply.github.com>
2024-06-03 13:02:40 -0300

    🚑 fix response json

--- src/http/controllers/users/get-user.ts
+++ src/http/controllers/users/get-user.ts
@@ -1,53 +1,51 @@
-import { prisma } from '@/lib/prisma'
-import { FastifyInstance } from 'fastify'
-import { ZodTypeProvider } from 'fastify-type-provider-zod'
-import z from 'zod'
-import { NotFoundError } from '../_errors/NotFound'
+import { prisma } from '@/lib/prisma';
+import { FastifyInstance } from 'fastify';
+import { ZodTypeProvider } from 'fastify-type-provider-zod';
+import z from 'zod';
+import { NotFoundError } from '../_errors/NotFound';
 
 export async function getUser(app: FastifyInstance) {
-    app.withTypeProvider<ZodTypeProvider>().get(
-        '/users/:userId',
-        {
-            schema: {
-                summary: 'Get an user',
-                tags: ['Auth'],
-                params: z.object({
-                    userId: z.coerce.number().int(),
-                }),
-                response: {
-                    200: z.object({
-                        user: z.object({
-                            nickname: z.string(),
-                            picture: z.string().url().nullable(),
-                            score: z.number().int(),
-                        })
-                    }),
-                },
-            },
+  app.withTypeProvider<ZodTypeProvider>().get(
+    '/users/:userId',
+    {
+      schema: {
+        summary: 'Get an user',
+        tags: ['Auth'],
+        params: z.object({
+          userId: z.coerce.number().int(),
+        }),
+        response: {
+          200: z.object({
+            nickname: z.string(),
+            picture: z.string().url().nullable(),
+            score: z.number().int(),
+          }),
         },
-        async (request, reply) => {
-            const { userId } = request.params
+      },
+    },
+    async (request, reply) => {
+      const { userId } = request.params;
 
-            const user = await prisma.user.findUnique({
-                select: {
-                    nickname: true,
-                    picture: true,
-                    score: true,
-                },
-                where: {
-                    id: userId,
-                }
-            })
+      const user = await prisma.user.findUnique({
+        select: {
+          nickname: true,
+          picture: true,
+          score: true,
+        },
+        where: {
+          id: userId,
+        },
+      });
 
-            if (user === null) {
-                throw new NotFoundError("User not found")
-            }
+      if (user === null) {
+        throw new NotFoundError('User not found');
+      }
 
-            return reply.send({ user: {
-                nickname: user.nickname,
-                picture: user.picture,
-                score: user.score
-            } })
-        }
-    )
+      return reply.send({
+        nickname: user.nickname,
+        picture: user.picture,
+        score: user.score,
+      });
+    }
+  );
 }
