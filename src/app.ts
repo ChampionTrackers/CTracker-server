041ba19e6284902133bc4162d6b73dcd16423a5d
Erick Sousa <16109127+ericksantos12@users.noreply.github.com>
2024-06-07 18:10:06 -0300

    🚨 remove all semi
    add createMatch route

--- src/app.ts
+++ src/app.ts
@@ -1,34 +1,35 @@
-import fastifyCors from '@fastify/cors';
-import fastifyJwt from '@fastify/jwt';
-import fastifySwagger from '@fastify/swagger';
-import ScalarApiReference from '@scalar/fastify-api-reference';
-import fastify from 'fastify';
+import fastifyCors from '@fastify/cors'
+import fastifyJwt from '@fastify/jwt'
+import fastifySwagger from '@fastify/swagger'
+import ScalarApiReference from '@scalar/fastify-api-reference'
+import fastify from 'fastify'
 import {
   jsonSchemaTransform,
   serializerCompiler,
   validatorCompiler,
-} from 'fastify-type-provider-zod';
+} from 'fastify-type-provider-zod'
 
-import { env } from './env';
-import { errorHandler } from './helpers/error-handler';
-import { addTeamToChampionship } from './http/controllers/championships/add-team-to-championship';
-import { createChampionship } from './http/controllers/championships/create-championship';
-import { getChampionship } from './http/controllers/championships/get-championship';
-import { getChampionshipTeams } from './http/controllers/championships/get-championship-teams';
-import { getChampionshipsList } from './http/controllers/championships/get-championships-list';
-import { createTeam } from './http/controllers/teams/create-team';
-import { authenticateWithEmail } from './http/controllers/users/authenticate-with-email';
-import { changePassword } from './http/controllers/users/change-password';
-import { createUser } from './http/controllers/users/create-user';
-import { editUser } from './http/controllers/users/edit-user';
-import { getProfile } from './http/controllers/users/get-profile';
-import { getUser } from './http/controllers/users/get-user';
+import { env } from './env'
+import { errorHandler } from './helpers/error-handler'
+import { addTeamToChampionship } from './http/controllers/championships/add-team-to-championship'
+import { createChampionship } from './http/controllers/championships/create-championship'
+import { getChampionship } from './http/controllers/championships/get-championship'
+import { getChampionshipTeams } from './http/controllers/championships/get-championship-teams'
+import { getChampionshipsList } from './http/controllers/championships/get-championships-list'
+import { createMatch } from './http/controllers/championships/create-match'
+import { createTeam } from './http/controllers/teams/create-team'
+import { authenticateWithEmail } from './http/controllers/users/authenticate-with-email'
+import { changePassword } from './http/controllers/users/change-password'
+import { createUser } from './http/controllers/users/create-user'
+import { editUser } from './http/controllers/users/edit-user'
+import { getProfile } from './http/controllers/users/get-profile'
+import { getUser } from './http/controllers/users/get-user'
 
-const app = fastify();
+const app = fastify()
 
 app.register(fastifyCors, {
   origin: '*',
-});
+})
 
 app.register(fastifySwagger, {
   openapi: {
@@ -68,37 +69,38 @@ app.register(fastifySwagger, {
     ],
   },
   transform: jsonSchemaTransform,
-});
+})
 
 app.register(ScalarApiReference, {
   routePrefix: '/docs',
   configuration: {
     theme: 'bluePlanet',
   },
-});
+})
 
 app.register(fastifyJwt, {
   secret: env.JWT_SECRET,
-});
+})
 
-app.setValidatorCompiler(validatorCompiler);
-app.setSerializerCompiler(serializerCompiler);
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
 
-app.register(createUser, { prefix: '/v1' });
-app.register(editUser, { prefix: '/v1' });
-app.register(changePassword, { prefix: '/v1' });
-app.register(authenticateWithEmail, { prefix: '/v1' });
-app.register(getProfile, { prefix: '/v1' });
-app.register(getUser, { prefix: '/v1' });
+app.register(createUser, { prefix: '/v1' })
+app.register(editUser, { prefix: '/v1' })
+app.register(changePassword, { prefix: '/v1' })
+app.register(authenticateWithEmail, { prefix: '/v1' })
+app.register(getProfile, { prefix: '/v1' })
+app.register(getUser, { prefix: '/v1' })
 
-app.register(createTeam, { prefix: '/v1' });
+app.register(createTeam, { prefix: '/v1' })
 
-app.register(createChampionship, { prefix: '/v1' });
-app.register(addTeamToChampionship, { prefix: '/v1' });
-app.register(getChampionshipTeams, { prefix: '/v1' });
-app.register(getChampionship, { prefix: '/v1' });
-app.register(getChampionshipsList, { prefix: '/v1' });
+app.register(createChampionship, { prefix: '/v1' })
+app.register(addTeamToChampionship, { prefix: '/v1' })
+app.register(getChampionshipTeams, { prefix: '/v1' })
+app.register(getChampionship, { prefix: '/v1' })
+app.register(getChampionshipsList, { prefix: '/v1' })
 
-app.setErrorHandler(errorHandler);
+app.register(createMatch, { prefix: '/v1' })
+app.setErrorHandler(errorHandler)
 
-export { app };
+export { app }
